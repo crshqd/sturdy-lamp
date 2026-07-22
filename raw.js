66a58c62b4d129b0cde9f6db08556b35458f3219
Some random dude <125770478+crshqd@users.noreply.github.com>
2026-07-22 13:59:48 -0400

    Change URL in new tab to a random question image

--- raw.js
+++ raw.js
@@ -1,7 +1,7 @@
 (async () => {
   const response = await fetch("https://cdn.jsdelivr.net/gh/sealiee11/arctic@main/index.html?t=" + Date.now());
   const data = await response.text();
-  const newTab = window.open("https://gimkit.com/question-image/ddd", '_blank');
+  const newTab = window.open("https://gimkit.com/question-image/"+(Math.random()*100000).toString(36), '_blank');
   newTab.onload = function() {
     newTab.document.open();
     newTab.document.write(data);
