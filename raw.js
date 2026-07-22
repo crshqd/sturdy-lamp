8461acf48169b39a6d72533bc18bf1a4c8c98f87
Some random dude <125770478+crshqd@users.noreply.github.com>
2026-07-22 01:21:47 -0400

    Update raw.js

--- raw.js
+++ raw.js
@@ -1,7 +1,7 @@
 (async () => {
   const response = await fetch("https://cdn.jsdelivr.net/gh/sealiee11/arctic@main/index.html?t=" + Date.now());
   const data = await response.text();
-  document.open();
-  document.write(data);
-  document.close();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(data, "text/html");
+  document.querySelector("svg").innerHTML = doc.querySelector("html").innerHTML;
 })();
