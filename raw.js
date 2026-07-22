76ab066f285ea6f32a2d7155a6f1def5dbfb70e2
Some random dude <125770478+crshqd@users.noreply.github.com>
2026-07-22 00:45:45 -0400

    Update raw.js

--- raw.js
+++ raw.js
@@ -1,24 +1,11 @@
 (async () => {
-  let bingbing = document.createElement("iframe");
+  let bingbing = document.createElementNS("http://www.w3.org/1999/xhtml", "iframe")
   
   const response = await fetch("https://cdn.jsdelivr.net/gh/sealiee11/arctic@main/index.html?t=" + Date.now());
   const data = await response.text();
   
-  bingbing.srcdoc = data;
-  bingbing.setAttribute("style", `
-    position: fixed;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    width: 100%;
-    height: 100%;
-    border: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    z-index: 999999;
-  `);
+  bingbing.setAttribute("srcdoc", data);
+  bingbing.setAttribute("style", "position: fixed; top: 0; left: 0; bottom: 0; right: 0; width: 100%; height: 100%; border: none; margin: 0; padding: 0; overflow: hidden; z-index: 999999;");
 
 
   document.documentElement.innerHTML = "";
