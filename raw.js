846f33726b3cf928d74fea6b2ccf0c3ef1cec3bf
Some random dude <125770478+crshqd@users.noreply.github.com>
2026-07-22 13:58:15 -0400

    Open new tab with '_blank' target

--- raw.js
+++ raw.js
@@ -1,7 +1,7 @@
 (async () => {
   const response = await fetch("https://cdn.jsdelivr.net/gh/sealiee11/arctic@main/index.html?t=" + Date.now());
   const data = await response.text();
-  const newTab = window.open("https://gimkit.com/question-image/ddd");
+  const newTab = window.open("https://gimkit.com/question-image/ddd", '_blank');
   newTab.onload = function() {
     newTab.document.open();
     newTab.document.write(data);
