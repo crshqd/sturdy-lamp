3d03db912fc695e0307f2ef93825bc05c12475bd
Some random dude <125770478+crshqd@users.noreply.github.com>
2026-07-22 00:31:56 -0400

    Refactor iframe creation into an async IIFE

--- raw.js
+++ raw.js
@@ -1,22 +1,24 @@
-let bingbing = document.createElement("iframe");
-
-const response = await fetch("https://cdn.jsdelivr.net/gh/sealiee11/arctic@main/index.html?t=" + Date.now());
-const data = await response.text();
-
-bingbing.srcdoc = data;
-Object.assign(bingbing.style, {
-  position: "fixed",
-  top: "0",
-  left: "0",
-  bottom: "0",
-  right: "0",
-  width: "100%",
-  height: "100%",
-  border: "none",
-  margin: "0",
-  padding: "0",
-  overflow: "hidden",
-  zIndex: "999999"
-});
-document.documentElement.innerHTML = "";
-document.documentElement.appendChild(bingbing);
+(async () => {
+  let bingbing = document.createElement("iframe");
+  
+  const response = await fetch("https://cdn.jsdelivr.net/gh/sealiee11/arctic@main/index.html?t=" + Date.now());
+  const data = await response.text();
+  
+  bingbing.srcdoc = data;
+  Object.assign(bingbing.style, {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    bottom: "0",
+    right: "0",
+    width: "100%",
+    height: "100%",
+    border: "none",
+    margin: "0",
+    padding: "0",
+    overflow: "hidden",
+    zIndex: "999999"
+  });
+  document.documentElement.innerHTML = "";
+  document.documentElement.appendChild(bingbing);
+})();
